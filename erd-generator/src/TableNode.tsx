402272e9ac647b7740225a84cd0bdf59f95656eb
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-25 17:52:40 -0500

    Removed handles

--- erd-generator/src/TableNode.tsx
+++ erd-generator/src/TableNode.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import { Position, NodeProps, BuiltInNode, Handle } from "@xyflow/react";
 
-export default function TableNode({
-  data,
-  isConnectable,
-  targetPosition = Position.Left,
-  sourcePosition = Position.Right,
-}: NodeProps<BuiltInNode>) {
+export default function TableNode({ data }: NodeProps<BuiltInNode>) {
   return (
     <>
-      <Handle
-        type="target"
-        position={targetPosition}
-        isConnectable={isConnectable}
-      />
-      {data?.label}
-      <Handle
-        type="source"
-        position={sourcePosition}
-        isConnectable={isConnectable}
-      />
+      <div style={{ textAlign: "center" }}>{data?.label}</div>
     </>
   );
 }
