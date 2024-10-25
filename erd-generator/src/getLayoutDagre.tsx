33da56e8433f81525911277b6b193a863a2d9f2c
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-25 13:02:40 -0500

    Renamed file

---
+++ erd-generator/src/getLayoutDagre.tsx
@@ -0,0 +1,48 @@
+import dagre from "dagre";
+import { Node, Edge, Position } from "@xyflow/react";
+
+const nodeWidth = 300;
+const nodeHeight = 400;
+
+const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (
+  nodes: Node[],
+  edges: Edge[],
+  direction: string = "TB"
+) => {
+  const isHorizontal = direction === "LR";
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  const newNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    console.log("Node with position:", nodeWithPosition);
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? Position.Left : Position.Top,
+      sourcePosition: isHorizontal ? Position.Right : Position.Bottom,
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+    };
+
+    return newNode;
+  });
+
+  return { nodes: newNodes, edges };
+};
+
+export default getLayoutedElements;
