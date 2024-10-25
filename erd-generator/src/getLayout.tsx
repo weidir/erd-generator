3360da63e1d162b6afcadae3ddafbae64bf2785e
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-25 11:09:08 -0500

    Updated to reflect documentation example more closely

--- erd-generator/src/getLayout.tsx
+++ erd-generator/src/getLayout.tsx
@@ -1,12 +1,12 @@
 import dagre from "dagre";
-import { Node, Position } from "@xyflow/react";
+import { Node, Edge, Position } from "@xyflow/react";
 
-const nodeWidth = 172;
-const nodeHeight = 36;
+const nodeWidth = 300;
+const nodeHeight = 400;
 
-const getLayoutedElements = (nodes: Node[], direction = "TB") => {
-  const dagreGraph = new dagre.graphlib.Graph();
-  dagreGraph.setDefaultEdgeLabel(() => ({}));
+const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (nodes, edges, direction = "TB") => {
   const isHorizontal = direction === "LR";
   dagreGraph.setGraph({ rankdir: direction });
 
@@ -14,27 +14,30 @@ const getLayoutedElements = (nodes: Node[], direction = "TB") => {
     dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
   });
 
-  dagreGraph.nodes().forEach((node) => {
-    dagreGraph.setNode(node, { width: nodeWidth, height: nodeHeight });
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
   });
 
   dagre.layout(dagreGraph);
 
-  nodes.forEach((node) => {
+  const newNodes = nodes.map((node) => {
     const nodeWithPosition = dagreGraph.node(node.id);
-    node.targetPosition = isHorizontal ? Position.Left : Position.Top;
-    node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    node.position = {
-      x: nodeWithPosition.x - nodeWidth / 2,
-      y: nodeWithPosition.y - nodeHeight / 2,
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? Position.Left : Position.Top,
+      sourcePosition: isHorizontal ? Position.Right : Position.Bottom,
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
     };
 
-    return node;
+    return newNode;
   });
 
-  return nodes;
+  return { nodes: newNodes, edges };
 };
 
 export default getLayoutedElements;
