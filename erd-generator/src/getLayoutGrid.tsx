5a1cb999ad0f6a97828e56f88557813b9c073740
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-26 19:53:44 -0500

    Implemented getMaxWidthHeight function

--- erd-generator/src/getLayoutGrid.tsx
+++ erd-generator/src/getLayoutGrid.tsx
@@ -1,10 +1,28 @@
-import { Node, Edge, Position } from "@xyflow/react";
+import { Node } from "@xyflow/react";
 
-const nodeWidth = 300;
-const nodeHeight = 400;
+// Get the maximum width and height of the nodes
+const getMaxWidthHeight = (nodes) => {
+  let maxWidth = 0;
+  let maxHeight = 0;
+  for (const node of nodes) {
+    let width = parseInt(node.style.width);
+    let height = parseInt(node.style.height);
+    if (width > maxWidth) {
+      maxWidth = width;
+    }
+    if (height > maxHeight) {
+      maxHeight = height;
+    }
+  }
+  return { maxWidth, maxHeight };
+};
 
 const getGridLayoutedElements = (nodes: Node[]) => {
   const size = nodes.length;
+
+  // Get the maximum width and height of the nodes
+  const { maxWidth, maxHeight } = getMaxWidthHeight(nodes);
+  console.log("Max Width:", maxWidth, "Max Height:", maxHeight);
 
   // Calculate the number of rows and columns
   const rows = Math.ceil(Math.sqrt(size));
@@ -16,8 +34,8 @@ const getGridLayoutedElements = (nodes: Node[]) => {
   const newNodes = nodes.map((node, index) => {
     const row = Math.floor(index / cols);
     const col = index % cols;
-    const x = col * nodeWidth + 50 * col;
-    const y = row * nodeHeight + 50 * row;
+    const x = col * maxWidth + 50 * col;
+    const y = row * maxHeight + 50 * row;
 
     return {
       ...node,
