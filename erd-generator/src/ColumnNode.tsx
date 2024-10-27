65b29162aec35b09ba6383669702e39efc714f35
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-27 15:45:40 -0500

    Initial commit

---
+++ erd-generator/src/ColumnNode.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import { NodeProps, BuiltInNode } from "@xyflow/react";
+
+// Define the BuiltInNode type with an index signature
+type BuiltInNode = {
+  label: string;
+  parent: string;
+  [key: string]: any; // Index signature to allow additional properties
+};
+
+// Define the NodeProps type
+type NodeProps<T> = {
+  data: T;
+};
+
+export default function ColumnNode({ data }: NodeProps<BuiltInNode>) {
+  return (
+    <div className="column-node" data-parentId={data?.parent}>
+      {data?.label}
+    </div>
+  );
+}
