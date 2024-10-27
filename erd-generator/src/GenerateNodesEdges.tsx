3c4caa4e292c1d81bd516bd998f10f30a501e07e
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-27 15:46:07 -0500

    Added icon, hover functions, other minor refactors

--- erd-generator/src/GenerateNodesEdges.tsx
+++ erd-generator/src/GenerateNodesEdges.tsx
@@ -15,6 +15,9 @@ export const GenerateTableNodesEdges = (parsedDbml: any) => {
 
     // Loop through each table in the parsed DBML
     for (const tableName in parsedDbml) {
+      // Set a variable for the table object
+      const table = parsedDbml[tableName];
+
       // Set the initial x and y position for each table
       const xPosition = 350 * index;
       const yPosition = 50 * index;
@@ -25,7 +28,47 @@ export const GenerateTableNodesEdges = (parsedDbml: any) => {
       const tableNode = {
         id: tableName,
         data: {
-          label: tableName,
+          label: (
+            <div
+              style={{ textAlign: "left" }}
+              data-tooltip-id="table-tool-tip"
+              data-tooltip-content={table.description}
+              data-tooltip-place="right"
+            >
+              {tableName}
+              {"   "}
+              {table.description && (
+                <svg
+                  width="13px"
+                  height="13px"
+                  viewBox="0 0 28 28"
+                  fill="none"
+                  stroke="white"
+                >
+                  <path
+                    d="M8 10.25C8 9.83579 8.33579 9.5 8.75 9.5H18.75C19.1642 9.5 19.5 9.83579 19.5 10.25C19.5 10.6642 19.1642 11 18.75 11H8.75C8.33579 11 8 10.6642 8 10.25Z"
+                    fill="#212121"
+                    stroke="white"
+                  />
+                  <path
+                    d="M8 14.75C8 14.3358 8.33579 14 8.75 14H18.75C19.1642 14 19.5 14.3358 19.5 14.75C19.5 15.1642 19.1642 15.5 18.75 15.5H8.75C8.33579 15.5 8 15.1642 8 14.75Z"
+                    fill="#212121"
+                    stroke="white"
+                  />
+                  <path
+                    d="M8.75 18.5C8.33579 18.5 8 18.8358 8 19.25C8 19.6642 8.33579 20 8.75 20H13.25C13.6642 20 14 19.6642 14 19.25C14 18.8358 13.6642 18.5 13.25 18.5H8.75Z"
+                    fill="#212121"
+                    stroke="white"
+                  />
+                  <path
+                    d="M14 2C14.4142 2 14.75 2.33579 14.75 2.75V4H18.5V2.75C18.5 2.33579 18.8358 2 19.25 2C19.6642 2 20 2.33579 20 2.75V4H20.75C21.9926 4 23 5.00736 23 6.25V19.2459C23 19.4448 22.921 19.6356 22.7803 19.7762L17.2762 25.2803C17.1355 25.421 16.9447 25.5 16.7458 25.5H6.75C5.50736 25.5 4.5 24.4926 4.5 23.25V6.25C4.5 5.00736 5.50736 4 6.75 4H8V2.75C8 2.33579 8.33579 2 8.75 2C9.16421 2 9.5 2.33579 9.5 2.75V4H13.25V2.75C13.25 2.33579 13.5858 2 14 2ZM6 6.25V23.25C6 23.6642 6.33579 24 6.75 24H15.9958V20.7459C15.9958 19.5032 17.0032 18.4959 18.2458 18.4959H21.5V6.25C21.5 5.83579 21.1642 5.5 20.75 5.5H6.75C6.33579 5.5 6 5.83579 6 6.25ZM18.2458 19.9959C17.8316 19.9959 17.4958 20.3317 17.4958 20.7459V22.9394L20.4393 19.9959H18.2458Z"
+                    fill="#212121"
+                    stroke="white"
+                  />
+                </svg>
+              )}
+            </div>
+          ),
         },
         position: { x: xPosition, y: yPosition },
         style: {
@@ -39,6 +82,7 @@ export const GenerateTableNodesEdges = (parsedDbml: any) => {
           // outline: "3px white solid",
         },
         type: "tableNode",
+        "data-tip": table.description,
       };
       nodes.push(tableNode);
 
@@ -88,7 +132,7 @@ export const GenerateTableNodesEdges = (parsedDbml: any) => {
                 strokeWidth: 2,
                 // stroke: '#FF0072',
               },
-              type: "start-end",
+              type: "custom-start-end",
               data: {
                 startLabel: startLabel,
                 endLabel: endLabel,
@@ -184,7 +228,7 @@ export const GenerateColumnNodesEdges = (
                 strokeWidth: 2,
                 stroke: "white",
               },
-              type: "start-end",
+              type: "custom-start-end",
               data: {
                 startLabel: startLabel,
                 endLabel: endLabel,
@@ -221,6 +265,9 @@ export const GenerateColumnNodesEdges = (
                     justifyContent: "space-between",
                     alignItems: "center",
                   }}
+                  data-tooltip-id="column-tool-tip"
+                  data-tooltip-content={column.note}
+                  data-tooltip-place="right"
                 >
                   {/* Field details */}
                   <span>
@@ -260,6 +307,36 @@ export const GenerateColumnNodesEdges = (
 		c31.234,0,60.598,12.163,82.684,34.249C466.34,109.841,466.34,184.025,420.748,229.617z"
                         />
                       </svg>
+                    )}{" "}
+                    {column.note && (
+                      <svg
+                        width="13px"
+                        height="13px"
+                        viewBox="0 0 28 28"
+                        fill="none"
+                        stroke="white"
+                      >
+                        <path
+                          d="M8 10.25C8 9.83579 8.33579 9.5 8.75 9.5H18.75C19.1642 9.5 19.5 9.83579 19.5 10.25C19.5 10.6642 19.1642 11 18.75 11H8.75C8.33579 11 8 10.6642 8 10.25Z"
+                          fill="#212121"
+                          stroke="white"
+                        />
+                        <path
+                          d="M8 14.75C8 14.3358 8.33579 14 8.75 14H18.75C19.1642 14 19.5 14.3358 19.5 14.75C19.5 15.1642 19.1642 15.5 18.75 15.5H8.75C8.33579 15.5 8 15.1642 8 14.75Z"
+                          fill="#212121"
+                          stroke="white"
+                        />
+                        <path
+                          d="M8.75 18.5C8.33579 18.5 8 18.8358 8 19.25C8 19.6642 8.33579 20 8.75 20H13.25C13.6642 20 14 19.6642 14 19.25C14 18.8358 13.6642 18.5 13.25 18.5H8.75Z"
+                          fill="#212121"
+                          stroke="white"
+                        />
+                        <path
+                          d="M14 2C14.4142 2 14.75 2.33579 14.75 2.75V4H18.5V2.75C18.5 2.33579 18.8358 2 19.25 2C19.6642 2 20 2.33579 20 2.75V4H20.75C21.9926 4 23 5.00736 23 6.25V19.2459C23 19.4448 22.921 19.6356 22.7803 19.7762L17.2762 25.2803C17.1355 25.421 16.9447 25.5 16.7458 25.5H6.75C5.50736 25.5 4.5 24.4926 4.5 23.25V6.25C4.5 5.00736 5.50736 4 6.75 4H8V2.75C8 2.33579 8.33579 2 8.75 2C9.16421 2 9.5 2.33579 9.5 2.75V4H13.25V2.75C13.25 2.33579 13.5858 2 14 2ZM6 6.25V23.25C6 23.6642 6.33579 24 6.75 24H15.9958V20.7459C15.9958 19.5032 17.0032 18.4959 18.2458 18.4959H21.5V6.25C21.5 5.83579 21.1642 5.5 20.75 5.5H6.75C6.33579 5.5 6 5.83579 6 6.25ZM18.2458 19.9959C17.8316 19.9959 17.4958 20.3317 17.4958 20.7459V22.9394L20.4393 19.9959H18.2458Z"
+                          fill="#212121"
+                          stroke="white"
+                        />
+                      </svg>
                     )}
                   </span>{" "}
                   <span style={{ color: rgba(153, 153, 153, 0.8) }}>
@@ -268,6 +345,7 @@ export const GenerateColumnNodesEdges = (
                 </div>
               </div>
             ),
+            parent: tableName,
           },
           position: { x: xPosition, y: yColPosition },
           style: {
@@ -281,7 +359,8 @@ export const GenerateColumnNodesEdges = (
           extent: "parent" as const,
           // sourcePosition: Position.Right,
           // targetPosition: Position.Left,
-          type: "tableNode",
+          type: "columnNode",
+          "data-tip": column.note,
         };
         nodes.push(columnNode);
       }
